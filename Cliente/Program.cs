2e7a6809768725b6f5dd7d685ea7b531ce4e446b
brunoquiroz <colobrunmon@gmail.com>
2022-05-11 00:59:04 -0400

    Agregar archivos de proyecto.

---
+++ Cliente/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace Cliente
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IPHostEntry host = Dns.GetHostEntry("localhost");
+            IPAddress iPAddress = host.AddressList[0];
+            IPEndPoint remoteEP = new IPEndPoint(iPAddress, 2050);
+
+
+
+            try
+            {
+                Socket sender = new Socket(iPAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+                sender.Connect(remoteEP);
+
+                Console.WriteLine("Conectado");
+                string comando = "";
+                while(comando != "chao")
+                {
+                    Console.WriteLine("Ingrese un mensaje para el servidor");
+                    comando = Console.ReadLine();
+                    byte[] msg = Encoding.ASCII.GetBytes(comando+"<EOF>");
+                    int byteSent = sender.Send(msg);
+                    byte[] bytes = new byte[1024];
+                    int byteRec = sender.Receive(bytes);
+                    string texto = Encoding.ASCII.GetString(bytes, 0, byteRec);
+                    Console.WriteLine("Servidor: " + texto);
+                }
+                sender.Shutdown(SocketShutdown.Both);
+                sender.Close();
+                
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
